d5d23cb05a20f4a6ae23f40ccd8a63aae4081376
Lucas Otoni <lotoni.vivo@gmail.com>
2022-09-09 21:00:02 -0300

    feature: requisito 4

--- src/routers/productsRoutes.router.js
+++ src/routers/productsRoutes.router.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const productsController = require('../controllers/products.controller');
+const validateNameOfProduct = require('../services/validations/validationNameValue');
 
 const router = express.Router();
 
 router.get('/', productsController.getProducts);
 
-router.post('/', productsController.postProduct);
-
 router.get('/:id', productsController.getProductById);
+
+router.post('/', validateNameOfProduct.validateName, productsController.postProduct);
 
 module.exports = router;
---
+++ src/services/validations/schemas.js
@@ -0,0 +1,9 @@
+// const Joi = require('joi');
+
+// const NameSchema = Joi.string().required();
+// const NameLenghtSchema = Joi.string().min(5);
+
+// module.exports = {
+//   NameSchema,
+//   NameLenghtSchema,
+// };
